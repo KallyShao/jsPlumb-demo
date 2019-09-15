94ee8a27d9b7c7014c32b795f4fd758afb6bf308
Kally <188963758@qq.com>
2019-09-15 21:58:48 +0800

    feat: m

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,109 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+    <div class="container-fluid">
+      <div class="row">
+        <div id="side-buttons" class="col-md-1 bg-info min-height">
+          <div>
+            <h5>节点类型列表</h5>
+            <a class="btn btn-success btn-controler" href="#" data-template="tpl-audio" role="button">
+              <i class="fa fa-play-circle-o" aria-hidden="true"></i>
+              语音播报</a>
+            <a class="btn btn-success btn-controler" href="#" data-template="tpl-menu" role="button">
+              <i class="fa fa-navicon" aria-hidden="true"></i>
+              菜单</a>
+           
+          </div>
+        </div>
+        <div class="col-md-11 bg-success min-height" id="drop-bg">
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import jsPlumb from 'jsplumb';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      areaId: '#drop-bg'
+    }
+  },
+  methods: {
+    // 主要入口
+    main() {
+      jsPlumb.setContainer('diagramContainer')
+
+        $('.btn-controler').draggable({
+          helper: 'clone',
+          scope: 'ss'
+        })
+
+        $(this.areaId).droppable({
+          scope: 'ss',
+          drop: function (event, ui) {
+            this.dropNode(ui.draggable[0].dataset.template, ui.position)
+          }
+        })
+
+        $('#app').on('click', function (event) {
+          event.stopPropagation()
+          event.preventDefault()
+          eventHandler(event.target.dataset)
+        })
+
+        // 单点击了连接线上的X号
+        jsPlumb.bind('dblclick', function (conn, originalEvent) {
+          DataDraw.deleteLine(conn)
+        })
+
+        // 当链接建立
+        jsPlumb.bind('beforeDrop', function (info) {
+          return connectionBeforeDropCheck(info)
+        })
+
+        // 让退出节点可拖动
+        // addDraggable(fixedNodeId.end)
+        // initBeginNode()
+        // initEndNode()
+
+        // DataProcess.inputData(data.nodeList)
+        DataDraw.draw(data.nodeList)
+    },
+    // 放入拖动的节点
+    dropNode(template, position) {
+      position.left -= $('#side-buttons').outerWidth()
+      position.id = uuid.v1()
+      position.generateId = uuid.v1
+      var html = renderHtml(template, position)
+
+      $(areaId).append(html)
+
+      this.initSetNode(template, position.id)
+    },
+      // 初始化节点设置
+    initSetNode (template, id) {
+      addDraggable(id)
+
+      if (template === 'tpl-audio') {
+        setEnterPoint(id)
+        setExitPoint(id)
+      } else if (template === 'tpl-menu') {
+        setEnterPoint(id + '-heading')
+        setExitMenuItem(id)
+      }
+    }
+  },
+  mounted() {
+    jsPlumb.ready(function() {
+      this.main();
+    });
   }
 }
 </script>
